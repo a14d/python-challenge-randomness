484946a9cf40b78fe85f310410b7c24fd8d53537
abderrahmane <abderrahmane.ayad@gmail.com>
2019-11-24 22:25:19 +0100

    add the randomness test

---
+++ randomness.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+import random
+
+START = 1
+END   = 50
+LIMIT = 11
+
+
+def get_lottery_number():
+    number = [random.randint(START, END) for _ in range(1, LIMIT)]
+    number.sort()
+    return number
+
+
+if __name__ == '__main__':
+    print(get_lottery_number())
